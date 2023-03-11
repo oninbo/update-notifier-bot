c332e1d2ae27e5e28db2b3fed9458cc10ac7a31f
Elena Lebedeva <oninbo@gmail.com>
2023-03-11 10:08:05 +0300

    Refactor

--- link-parser/src/test/java/ru/tinkoff/edu/java/link_parser/configuration/ApplicationConfig.java
+++ link-parser/src/test/java/ru/tinkoff/edu/java/link_parser/configuration/ApplicationConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:app.properties")
-@ComponentScan("ru.tinkoff.edu.java.link_parser.github")
-@ComponentScan("ru.tinkoff.edu.java.link_parser.stackoverflow")
-@ComponentScan("ru.tinkoff.edu.java.link_parser")
+@PropertySource("classpath:application.properties")
+@ComponentScan("ru.tinkoff.edu.java")
 public class ApplicationConfig {}
