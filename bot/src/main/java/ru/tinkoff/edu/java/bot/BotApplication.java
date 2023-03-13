734a99c9d13cea68abba6bc81ee90b0c3ce70bda
Elena Lebedeva <oninbo@gmail.com>
2023-03-13 15:04:07 +0300

    Add LinkParserResultPrinter

--- bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
@@ -21,8 +21,8 @@ public class BotApplication {
         // В hw1 не требуется этого делать, но, возможно, понадобится в будущем, да и просто попробовать было интересно
         var linkParserService = ctx.getBean(LinkParserService.class);
         LinkParserResult result = linkParserService.parse("https://github.com/abc/xyz");
-        var visitor = ctx.getBean(BotLinkParserResultVisitor.class);
-        result.acceptVisitor(visitor);
+        var printer = ctx.getBean(LinkParserResultPrinter.class);
+        printer.print(result);
 
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
         System.out.println(config);
--- bot/src/main/java/ru/tinkoff/edu/java/bot/LinkParserResultPrinter.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/LinkParserResultPrinter.java
@@ -1,12 +1,17 @@
 package ru.tinkoff.edu.java.bot;
 
 import org.springframework.stereotype.Component;
+import ru.tinkoff.edu.java.link_parser.LinkParserResult;
 import ru.tinkoff.edu.java.link_parser.LinkParserResultVisitor;
 import ru.tinkoff.edu.java.link_parser.github.GitHubParserResult;
 import ru.tinkoff.edu.java.link_parser.stackoverflow.StackOverflowParserResult;
 
 @Component
-public class BotLinkParserResultVisitor implements LinkParserResultVisitor {
+public class LinkParserResultPrinter implements LinkParserResultVisitor {
+    public void print(LinkParserResult result) {
+        result.acceptVisitor(this);
+    }
+
     @Override
     public void visit(GitHubParserResult gitHubParserResult) {
         System.out.println("userName: " + gitHubParserResult.userName() +
