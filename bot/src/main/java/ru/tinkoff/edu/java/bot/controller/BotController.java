f189ad42b228f2639d31110711dff547a6a10897
Elena Lebedeva <oninbo@gmail.com>
2023-03-27 22:20:51 +0300

    Refactor BotController

--- bot/src/main/java/ru/tinkoff/edu/java/bot/controller/BotController.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/controller/BotController.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.edu.java.bot.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,9 @@ import ru.tinkoff.edu.java.link_parser.LinkParserService;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 public class BotController {
     private final LinkParserService linkParserService;
-
-    public BotController(LinkParserService linkParserService) {
-        this.linkParserService = linkParserService;
-    }
 
     @PostMapping("/updates")
     public void updates(@Valid @RequestBody LinkUpdateRequest linkUpdateRequest) {
