a341533ce097580f6a27431acf99d6d6c4a27323
Elena Lebedeva <oninbo@gmail.com>
2023-03-27 22:22:55 +0300

    Refactor LinkUpdaterScheduler

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/scheduler/LinkUpdaterScheduler.java
@@ -1,24 +1,19 @@
 package ru.tinkoff.edu.java.scrapper.scheduler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ru.tinkoff.edu.java.scrapper.configuration.SchedulerConfig;
 
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class LinkUpdaterScheduler {
-    private final Logger logger;
-
     private final SchedulerConfig schedulerConfig;
-
-    public LinkUpdaterScheduler(SchedulerConfig schedulerConfig) {
-        this.schedulerConfig = schedulerConfig;
-        logger = LoggerFactory.getLogger(LinkUpdaterScheduler.class);
-    }
 
     @Scheduled(fixedDelayString = "#{@schedulerConfig.getInterval()}")
     public void update() {
-        logger.info(String.format("Update each %s seconds!", schedulerConfig.getInterval().toSeconds()));
+        log.info(String.format("Update each %s seconds!", schedulerConfig.getInterval().toSeconds()));
     }
 }
