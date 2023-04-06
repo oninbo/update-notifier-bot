b909cfcf2350d84a143e48796eacb9a9e9b0b874
Elena Lebedeva <oninbo@gmail.com>
2023-04-06 21:11:16 +0300

    Refactor

--- scrapper/migrations/tables/003_create_github_repositories.sql
+++ scrapper/migrations/tables/003_create_github_repositories.sql
@@ -1,11 +1,11 @@
 CREATE TABLE github_repositories (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     name text NOT NULL,
-    user_name text NOT NULL,
+    username text NOT NULL,
     created_at timestamp NOT NULL DEFAULT now(),
     updated_at timestamp,
     CONSTRAINT github_repositories_pk PRIMARY KEY (id),
-    CONSTRAINT unique_name_user UNIQUE (name, user_name)
+    CONSTRAINT unique_name_user UNIQUE (name, username)
 );
 
 ALTER TABLE links
