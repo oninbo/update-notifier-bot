300a97d9fb8175557a2f6d2522ab5c5859de9e0d
oninbo <oninbo@gmail.com>
2023-04-10 09:56:39 +0300

    Change migrations

--- scrapper/migrations/tables/001_create_tg_chats.sql
+++ scrapper/migrations/tables/001_create_tg_chats.sql
@@ -4,8 +4,3 @@ CREATE TABLE tg_chats (
    CONSTRAINT unique_chat_id UNIQUE (chat_id),
    CONSTRAINT tg_chats_pk PRIMARY KEY (id)
 );
-
--- noinspection SqlAddNotNullColumn
-ALTER TABLE links
-    ADD COLUMN tg_chat_id uuid NOT NULL,
-    ADD CONSTRAINT tg_chat_id_fk FOREIGN KEY (tg_chat_id) REFERENCES tg_chats (id);
