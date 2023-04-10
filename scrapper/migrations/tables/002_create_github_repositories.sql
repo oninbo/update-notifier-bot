c359ea393d3afbdff126a72795b6116c08984364
Elena Lebedeva <oninbo@gmail.com>
2023-04-11 02:25:00 +0300

    Change timestamp to timestamptz

--- scrapper/migrations/tables/002_create_github_repositories.sql
+++ scrapper/migrations/tables/002_create_github_repositories.sql
@@ -2,8 +2,8 @@ CREATE TABLE github_repositories (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     name text NOT NULL,
     username text NOT NULL,
-    created_at timestamp NOT NULL DEFAULT now(),
-    updated_at timestamp,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz,
     CONSTRAINT github_repositories_pk PRIMARY KEY (id),
     CONSTRAINT unique_name_user UNIQUE (name, username)
 );
